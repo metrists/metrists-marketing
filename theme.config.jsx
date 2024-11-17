eb3adc9ee2403432599de71dadeada52a2f4f5ce
parsasi <mail@parsa.pro>
2024-11-17 14:10:18 -0800

    Added Metrists logo to the docs pages

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import { Logo } from "./data/logo";
+
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: (
+    <Box w="36px">
+      <Logo />
+    </Box>
+  ),
   project: {
     link: "https://github.com/metrists",
   },
